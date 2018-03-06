67d7e55d60d32061a32ac20d15ad23211ada6d50
WilChrist <willynzesseu@gmail.com>
2018-03-06 22:05:52 +0000

    Template de base

---
+++ EnsasWare/App.xaml.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace EnsasWare
+{
+    /// <summary>
+    /// Integration of the logic off App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+}
