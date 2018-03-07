e3537ac64215d3e419c488e10cd6653297f57579
leroifrancis <leroifrancis56@gmail.com>
2018-03-07 01:08:53 +0000

    premiercommitgestionfiliere

---
+++ EnsasWare/GestionFiliere.xaml.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace EnsasWare
+{
+    /// <summary>
+    /// Logique d'interaction pour GestionFiliere.xaml
+    /// </summary>
+    public partial class GestionFiliere : UserControl
+    {
+        public GestionFiliere()
+        {
+            InitializeComponent();
+        }
+
+        private void ajouterFil_Click(object sender, RoutedEventArgs e)
+        {
+            //ajouter une filière
+        }
+
+        private void modifierFil_Click(object sender, RoutedEventArgs e)
+        {
+            //modifier une filière
+        }
+
+        private void supprimerFil_Click(object sender, RoutedEventArgs e)
+        {
+            //supprimer une filire
+        }
+
+        private void radfil_Loaded(object sender, RoutedEventArgs e)
+        {
+            //chargement des données
+            //DataClasses1DataContext bd = new DataClasses1DataContext();
+            //radfil.ItemsSource = bd.Filiere;
+        }
+    }
+}
